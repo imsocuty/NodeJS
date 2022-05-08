f31c6a686103ad2acfe75fc5c40e0a1b36bd92be
IMSOSEXY <ppppp4710@naver.com>
2022-05-08 23:57:41 +0900

    Express & js

--- models/users.js
+++ models/users.js
@@ -1,9 +1,9 @@
 let mysqlDB = require('../mysql-db');
 
 export default class Users {
-    static getUsers(params:Array<String>) {
+    static getUsers(params) {
         return new Promise((resolve, reject) => {
-            mysqlDB.query('select * from test_user where id=? and pw=?', params, function (err:object, rows:Array<object>, fields:object) {
+            mysqlDB.query('select * from test_user where id=? and pw=?', params, function (err, rows, fields) {
                 if (err) {
                     console.log(err);
                     reject(err);
--- project name (default is express-gen-ts)/build.ts
+++ project name (default is express-gen-ts)/build.ts
@@ -2,6 +2,7 @@
  * Remove old files, copy front-end ones.
  */
 
+// @ts-ignore
 import fs from 'fs-extra';
 import logger from 'jet-logger';
 import childProcess from 'child_process';
